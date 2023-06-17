c34309cd613f51e0aa5590d64be2d36256fb2f8d
Kurnia Cahya Febryanto <kcf170202@gmail.com>
2023-06-17 22:25:38 +0700

    feat: admin attendance

--- resources/views/admin/sheet-report.blade.php
+++ resources/views/admin/sheet-report.blade.php
@@ -1,129 +1,77 @@
 @extends('layouts.master')
+
+@section('css')
+    <!-- Bootstrap 5 CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/5.0.0-beta1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom Styles -->
+    <style>
+        .card {
+            border-radius: 20px;
+            box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+        }
+        .table th, .table td {
+            vertical-align: middle;
+        }
+    </style>
+@endsection
+
 @section('content')
-
-<div class="card">
-    <div class="card-header bg-success text-white">
-        Attendance Sheet Report
-    </div>
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-md table-hover" id="printTable">
-                <thead class="thead-dark">
+    <div class="card mt-5 shadow">
+        <div class="card-header bg-success text-white">
+            <h4 class="card-title mb-0">Attendance Sheet Report</h4>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped table-hover table-sm">
+                    <thead class="thead-dark">
                     <tr>
-
                         <th>Employee</th>
                         <th>Position</th>
-                        <!-- <th>ID</th> -->
-                        <!--   -->
+
                         @php
-                        $today = today();
-                        $dates = [];
-
-                        for ($i = 1; $i < $today->daysInMonth + 1; ++$i) {
-                            $dates[] = \Carbon\Carbon::createFromDate($today->year, $today->month, $i)->format('Y-m-d');
+                            $today = today();
+                            $dates = [];
+                            for ($i = 1; $i < $today->daysInMonth + 1; ++$i) {
+                                $dates[] = \Carbon\Carbon::createFromDate($today->year, $today->month, $i)->format('Y-m-d');
                             }
+                        @endphp
 
-                            @endphp
-                            @foreach ($dates as $date)
-                            <th style="">
-
-
-                                {{ $date }}
-
-                            </th>
-
-
-                            @endforeach
-
+                        @foreach ($dates as $date)
+                            <th>{{ $date }}</th>
+                        @endforeach
                     </tr>
-                </thead>
-
-                <tbody>
-
-
-
-
-
+                    </thead>
+                    <tbody>
                     @foreach ($employees as $employee)
+                        <tr>
+                            <td>{{ $employee->name }}</td>
+                            <td>{{ $employee->position }}</td>
 
-                    <input type="hidden" name="emp_id" value="{{ $employee->id }}">
-
-                    <tr>
-                        <td>{{ $employee->name }}</td>
-                        <td>{{ $employee->position }}</td>
-                        <!-- <td>{{ $employee->id }}</td> -->
-                        <!--   -->
-
-
-
-
-
-
-                        @for ($i = 1; $i < $today->daysInMonth + 1; ++$i)
-
-
-                            @php
-
-                            $date_picker = \Carbon\Carbon::createFromDate($today->year, $today->month, $i)->format('Y-m-d');
-
-                            $check_attd = \App\Models\Attendance::query()
-                            ->where('emp_id', $employee->id)
-                            ->where('attendance_date', $date_picker)
-                            ->first();
-
-                            $check_leave = \App\Models\Leave::query()
-                            ->where('emp_id', $employee->id)
-                            ->where('leave_date', $date_picker)
-                            ->first();
-
-                            @endphp
-                            <td>
-
-                                <div class="form-check form-check-inline ">
-
+                            @for ($i = 1; $i < $today->daysInMonth + 1; ++$i)
+                                @php
+                                    $date_picker = \Carbon\Carbon::createFromDate($today->year, $today->month, $i)->format('Y-m-d');
+                                    $check_attd = \App\Models\Attendance::query()->where('emp_id', $employee->id)->where('attendance_date', $date_picker)->first();
+                                    $check_leave = \App\Models\Leave::query()->where('emp_id', $employee->id)->where('leave_date', $date_picker)->first();
+                                @endphp
+                                <td>
                                     @if (isset($check_attd))
-                                    @if ($check_attd->status==1)
-                                    <i class="fa fa-check text-success"></i>
+                                        <i class="fa fa-check {{ $check_attd->status == 1 ? 'text-success' : 'text-danger' }}"></i>
                                     @else
-                                    <i class="fa fa-check text-danger"></i>
+                                        <i class="fas fa-times text-danger"></i>
                                     @endif
-
-                                    @else
-                                    <i class="fas fa-times text-danger"></i>
-                                    @endif
-                                </div>
-                                <div class="form-check form-check-inline">
 
                                     @if (isset($check_leave))
-                                    @if ($check_leave->status==1)
-                                    <i class="fa fa-check text-success"></i>
+                                        <i class="fa fa-check {{ $check_leave->status == 1 ? 'text-success' : 'text-danger' }}"></i>
                                     @else
-                                    <i class="fa fa-check text-danger"></i>
+                                        <i class="fas fa-times text-danger"></i>
                                     @endif
-
-                                    @else
-                                    <i class="fas fa-times text-danger"></i>
-                                    @endif
-
-
-                                </div>
-
-                            </td>
-
+                                </td>
                             @endfor
-                    </tr>
+                        </tr>
                     @endforeach
-
-
-
-
-
-                </tbody>
-                <!--   -->
-
-
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
 @endsection
