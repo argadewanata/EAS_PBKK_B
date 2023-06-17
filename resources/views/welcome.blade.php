461a289343df9f1df503aa6e195cabe178453d2b
Arga Dewanata <rerearga@gmail.com>
2023-06-17 12:32:39 +0700

    REVAMP: welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,23 @@
 @include('layouts.welcome')
-  
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-        <div class="top-right links color-white">
-            @auth
-            <a href="{{ url('/admin') }}">Admin</a>
-            @else
-            <a class="btn btn-secondary btn-md" style="color: white" href="{{ route('login') }}">Go To Login Panel</a>
 
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
-            @endif
-            @endauth
-        </div>
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+    <div class="top-right links color-white">
+        @auth
+        <a href="{{ url('/admin') }}">Admin</a>
+        @else
+
+        @if (Route::has('register'))
+        <a href="{{ route('register') }}">Register</a>
         @endif
+        @endauth
+    </div>
+    @endif
 
-        <div class="content">
-            <div class="title m-b-md">
-                <h1 class="text-center">Welcome to Simple Attendance System</h5>
-                <div class="clockStyle" id="clock">123</div>
-            </div>
-
-            
-        </div>
+    <div style="text-align: center;">
+        <img src="assets/images/Lambang ITS putih-05.png" alt="Logo ITS" width="300" height="300"></img>
+        <h1 style="color:white">ITS Attendance System</h1>
+        <a class="btn btn-outline-light btn-lg" style="color: white" href="{{ route('login') }}">Login</a>
     </div>
 
+</div>
--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,15 @@ use App\Http\Controllers\FingerDevicesControlller;
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
-Route::get('attended/{user_id}', '\App\Http\Controllers\AttendanceController@attended' )->name('attended');
-Route::get('attended-before/{user_id}', '\App\Http\Controllers\AttendanceController@attendedBefore' )->name('attendedBefore');
+Route::get('attended/{user_id}', '\App\Http\Controllers\AttendanceController@attended')->name('attended');
+Route::get('attended-before/{user_id}', '\App\Http\Controllers\AttendanceController@attendedBefore')->name('attendedBefore');
 Auth::routes(['register' => false, 'reset' => false]);
 
 Route::group(['middleware' => ['auth', 'Role'], 'roles' => ['admin']], function () {
     Route::resource('/employees', '\App\Http\Controllers\EmployeeController');
     Route::resource('/employees', '\App\Http\Controllers\EmployeeController');
     Route::get('/attendance', '\App\Http\Controllers\AttendanceController@index')->name('attendance');
-  
+
     Route::get('/latetime', '\App\Http\Controllers\AttendanceController@indexLatetime')->name('indexLatetime');
     Route::get('/leave', '\App\Http\Controllers\LeaveController@index')->name('leave');
     Route::get('/overtime', '\App\Http\Controllers\LeaveController@indexOvertime')->name('indexOvertime');
@@ -27,8 +27,8 @@ Route::group(['middleware' => ['auth', 'Role'], 'roles' => ['admin']], function 
 
     Route::get('/check', '\App\Http\Controllers\CheckController@index')->name('check');
     Route::get('/sheet-report', '\App\Http\Controllers\CheckController@sheetReport')->name('sheet-report');
-    Route::post('check-store','\App\Http\Controllers\CheckController@CheckStore')->name('check_store');
-    
+    Route::post('check-store', '\App\Http\Controllers\CheckController@CheckStore')->name('check_store');
+
     // Fingerprint Devices
     Route::resource('/finger_device', '\App\Http\Controllers\BiometricDeviceController');
 
@@ -44,8 +44,6 @@ Route::group(['middleware' => ['auth', 'Role'], 'roles' => ['admin']], function 
 
         return back();
     })->name('finger_device.clear.attendance');
-    
-
 });
 
 Route::group(['middleware' => ['auth']], function () {
@@ -54,7 +52,7 @@ Route::group(['middleware' => ['auth']], function () {
 
 
 
-    
+
 
 });
 
